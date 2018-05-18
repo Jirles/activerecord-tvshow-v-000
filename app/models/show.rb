762172662b70692d32c3377051339a7fe9a84765
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 02:07:47 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,31 +1,31 @@
 class Show < ActiveRecord::Base 
   
-  def highest_rating
-    Show.maximum("rating")
+  def self.highest_rating
+    self.maximum("rating")
   end 
   
-  def most_popular_show
-    Show.where(rating: highest_rating).first 
+  def self.most_popular_show
+    self.where(rating: highest_rating).first 
   end 
   
-  def lowest_rating
-    Show.minimum("rating")
+  def self.lowest_rating
+    self.minimum("rating")
   end 
   
-  def least_popular_show
-    Show.where(rating: lowest_rating).first
+  def self.least_popular_show
+    self.where(rating: lowest_rating).first
   end 
   
-  def ratings_sum
-    Show.sum("rating")
+  def self.ratings_sum
+    self.sum("rating")
   end 
   
-  def popular_shows
-    Show.where("rating > 5")
+  def self.popular_shows
+    self.where("rating > 5")
   end 
   
-  def shows_by_alphabetical_order
-    Show.order(:name)
+  def self.shows_by_alphabetical_order
+    self.order(:name)
   end 
   
 end 
